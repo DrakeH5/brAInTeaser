6885a7746c04a95f5efd1ce8a144af1ec0e303c6
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2023-05-06 18:02:38 -0500

    interface with python on backend setup

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,21 @@ const app = express()
 const cors = require("cors");
 app.use(cors());
 var bodyParser = require('body-parser')
+const { spawn } = require('child_process');
 
 app.get("/", (req, res) => {
   res.json("Welcome to the backend")
 })
 
 app.post("/generateQuestions", bodyParser.json(), (req, res) => {
-  res.json(req.body.input)
+  const algorithm = spawn('python', ['algo.py']);
+  algorithm.stdout.on('data', function(data) {
+      res.json(String(data))
+      //console.log(String(data))
+  })
+  //res.json(req.body.input)
 })
+
 
 
 app.listen(5000, () => { console.log("Server started on port 5000") })
