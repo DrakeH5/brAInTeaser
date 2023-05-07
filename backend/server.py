314a280eb62bb998984da721078e396971728e21
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2023-05-07 16:08:11 -0500

    remove parts to create questions

--- backend/server.py
+++ backend/server.py
@@ -4,18 +4,36 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-from summarizer import Summarizer
+from rake_nltk import Rake
+from textblob import TextBlob
+import re
 
-model = Summarizer()
+r = Rake()
+
+
+#from summarizer import Summarizer
+
+#model = Summarizer()
 
 @app.route("/generateQuestions", methods=["POST"])
 @cross_origin()
 def generateQuestions():
     input = eval(request.data.decode('UTF-8'))["input"]
-    result = model(input, min_length=6, max_length = 100, ratio = 0.4)
-    summarized_text = ''.join(result)
-    print (summarized_text)
-    return [summarized_text]
+    #result = model(input, min_length=6, max_length = 100, ratio = 0.4)
+    #summarized_text = ''.join(result)
+    #print (input)
+    #r.extract_keywords_from_text(input)
+    #scored = r.get_ranked_phrases()
+    sentences = re.split('[^0-9]["."][^0-9]', input)
+    questions = []
+    for sent in sentences: 
+        blob = TextBlob(sent)
+        questions.append(sent.lower().replace(blob.noun_phrases[0], "___"))
+        #r.extract_keywords_from_text(sent)
+        #questions.append(sent.lower().replace(r.get_ranked_phrases()[0], "___"))
+    print(questions)
+    return [questions]
+    #return [input]
 
 if __name__ == "__main__":
     app.run(debug=True)
