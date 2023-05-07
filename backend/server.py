fb0ba81c78443c445c9120b96aba03d3b864ff29
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2023-05-06 19:09:11 -0500

    narrow down input

--- backend/server.py
+++ backend/server.py
@@ -4,11 +4,18 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+from summarizer import Summarizer
+
+model = Summarizer()
+
 @app.route("/generateQuestions", methods=["POST"])
 @cross_origin()
 def generateQuestions():
     input = eval(request.data.decode('UTF-8'))["input"]
-    return [input]
+    result = model(input, min_length=6, max_length = 100, ratio = 0.4)
+    summarized_text = ''.join(result)
+    print (summarized_text)
+    return [summarized_text]
 
 if __name__ == "__main__":
     app.run(debug=True)
